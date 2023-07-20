7d4af121a1c23035a4b013857e98756635db5689
sunil <sunilkumar.ipm@gmail.com>
2023-07-20 12:40:48 +0530

    aws_route to public

---
+++ main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "test" {
+  ami = ami-03265a0778a880afb
+  instance_type = var.instance_type
+  subnet_id = var.subnet_id
+}
+
+
+variable "instance_type" {}
+variable "subnet_id" {}
