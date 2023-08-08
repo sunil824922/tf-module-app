edeb0b49a2c3b3fdc686746c20a654db3b86832c
sunil <sunilkumar.ipm@gmail.com>
2023-08-08 15:48:02 +0530

    health check

--- main.tf
+++ main.tf
@@ -79,8 +79,8 @@ resource "aws_lb_target_group" "main" {
     enabled = true
     healthy_threshold = 2
     unhealthy_threshold = 2
-    interval = 5
-    timeout = 4
+    interval = 6
+    timeout = 5
     path = "/health"
   }
 }
